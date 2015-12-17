df431da23e40b05209e3941538545ac72e227089
Alex <Alex@10.1.32.218>
2015-12-17 11:00:55 -0600

    Basic ArrayList

--- ShoppingList/src/shoppinglist/ShoppingList.java
+++ ShoppingList/src/shoppinglist/ShoppingList.java
@@ -1,5 +1,5 @@
 package shoppinglist;
-
+import java.util.ArrayList;
 /**
  *
  * @author change to your name or loose 5 points
@@ -10,7 +10,16 @@ public class ShoppingList {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // code application logic here
+        ArrayList ShoppingList= new ArrayList();
+        
+        ShoppingList.add("Tortillas");
+        ShoppingList.add("Avocados (For The Guacamole)");
+        ShoppingList.add("Steak");
+        ShoppingList.add("Tomatoes");
+        ShoppingList.add("Cheese");
+        ShoppingList.add("Refried Beans");
+        
+        System.out.println(ShoppingList);
     }
     
 }
